68f5b88c7d7114193c1a67de3c220296ba9aa9f8
Anton McConville <anton@hickory.ca>
2017-12-28 16:29:17 -0500

    beginning and reworking booklet

---
+++ secretmap/BookletController.swift
@@ -0,0 +1,125 @@
+//
+//  BookletController.swift
+//  secretmap
+//
+//  Created by Anton McConville on 2017-12-18.
+//  Copyright © 2017 Anton McConville. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class BookletController: UIViewController, UIPageViewControllerDataSource {
+    
+    // MARK: - Variables
+    private var pageViewController: UIPageViewController?
+    
+    // Initialize it right away here
+    private let contentImages = ["blockchain",
+                                 "footprint",
+                                 "watson",
+                                 "boy pirate",
+                                 "girl pirate"]
+    
+    // MARK: - View Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        createPageViewController()
+        setupPageControl()
+    }
+    
+    private func createPageViewController() {
+        
+        let pageController = self.storyboard!.instantiateViewController(withIdentifier: "booklet") as! UIPageViewController
+        pageController.dataSource = self
+        
+        if contentImages.count > 0 {
+            let firstController = getItemController(itemIndex: 0)!
+            let startingViewControllers = [firstController]
+            pageController.setViewControllers(startingViewControllers, direction: UIPageViewControllerNavigationDirection.forward, animated: false, completion: nil)
+        }
+        
+        pageViewController = pageController
+        addChildViewController(pageViewController!)
+        self.view.addSubview(pageViewController!.view)
+        pageViewController!.didMove(toParentViewController: self)
+    }
+    
+    private func setupPageControl() {
+        let appearance = UIPageControl.appearance()
+        appearance.pageIndicatorTintColor = UIColor.gray
+        appearance.currentPageIndicatorTintColor = UIColor.white
+        appearance.backgroundColor = UIColor.darkGray
+    }
+    
+    // MARK: - UIPageViewControllerDataSource
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        
+        let itemController = viewController as! BookletItemController
+        
+        if itemController.itemIndex > 0 {
+            return getItemController(itemIndex: itemController.itemIndex-1)
+        }
+        
+        return nil
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        
+        let itemController = viewController as! BookletItemController
+        
+        if itemController.itemIndex+1 < contentImages.count {
+            return getItemController(itemIndex: itemController.itemIndex+1)
+        }
+        
+        return nil
+    }
+    
+    private func getItemController(itemIndex: Int) -> BookletItemController? {
+        
+        if itemIndex < contentImages.count {
+            let pageItemController = self.storyboard!.instantiateViewController(withIdentifier: "ItemController") as! BookletItemController
+            pageItemController.itemIndex = itemIndex
+            pageItemController.imageName = contentImages[itemIndex]
+            return pageItemController
+        }
+        
+        return nil
+    }
+    
+    // MARK: - Page Indicator
+    
+    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        return contentImages.count
+    }
+    
+    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+        return 0
+    }
+    
+    // MARK: - Additions
+    
+    func currentControllerIndex() -> Int {
+        
+        let pageItemController = self.currentController()
+        
+        if let controller = pageItemController as? BookletItemController {
+            return controller.itemIndex
+        }
+        
+        return -1
+    }
+    
+    func currentController() -> UIViewController? {
+        
+        let count:Int = (self.pageViewController?.viewControllers?.count)!;
+        
+        if count > 0 {
+            return self.pageViewController?.viewControllers![0]
+        }
+        
+        return nil
+    }
+}
---
+++ secretmap/BookletItemController.swift
@@ -0,0 +1,33 @@
+//
+//  BookletItemController.swift
+//  secretmap
+//
+//  Created by Anton McConville on 2017-12-27.
+//  Copyright © 2017 Anton McConville. All rights reserved.
+//
+
+import UIKit
+
+class BookletItemController: UIViewController {
+    
+    // MARK: - Variables
+    var itemIndex: Int = 0
+    var imageName: String = "" {
+        
+        didSet {
+            
+            if let imageView = contentImageView {
+                imageView.image = UIImage(named: imageName)
+            }
+            
+        }
+    }
+    
+    @IBOutlet var contentImageView: UIImageView?
+    
+    // MARK: - View Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        contentImageView!.image = UIImage(named: imageName)
+    }
+}
